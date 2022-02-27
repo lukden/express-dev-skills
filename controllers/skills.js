831898c55de37a0637216a6907a801a7a01ea67b
lukden <dean.l@northeastern.edu>
2022-02-27 17:16:54 -0500

    updated lab

--- controllers/skills.js
+++ controllers/skills.js
@@ -10,6 +10,36 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  console.log(req.params.id)
+  skillsDb.findById(req.params.id, function (error, skill) {
+    res.render('skills/show', {
+      skill: skill,
+      error: error
+    })
+  })
+}
+
+function newSkill(req, res) {
+  res.render('skills/new')
+}
+
+function create(req, res) {
+  skillsDb.create(req.body, function (error, skill) {
+    res.redirect('/skills')
+  })
+}
+
+function deleteSkill(req, res) {
+  skillsDb.findByIdAndDelete(req.params.id, function(error, skill) {
+    res.redirect('/skills')
+  })
+}
+
 export {
-  index
+  index,
+  show,
+  newSkill as new,
+  create,
+  deleteSkill as delete
 }
--- routes/skills.js
+++ routes/skills.js
@@ -1,22 +1,20 @@
 import { Router } from 'express'
-import * as skillsCtrl from '../data/skills.js'
+import * as skillsCtrl from '../controllers/skills.js'
 const router = Router()
 
 /* GET users listing. */
 
-router.get('/', skillsCtrl)
+router.get('/', skillsCtrl.index)
+router.get("/new", skillsCtrl.new)
+router.get("/:id", skillsCtrl.show)
+
 router.get('/', function(req, res) {
   res.send('respond with a resource')
 })
 
-app.get('/skills', function(req, res) {
-  todoDb.find({}, function(error, skills) {
-    res.render('skills/index', {
-      skills: skills,
-      error: error
-    })
-  })
-})
+router.post("/", skillsCtrl.create)
+
+router.delete("/:id", skillsCtrl.delete)
 
 export {
   router
